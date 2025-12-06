3c19bf45df9e08bc644b5ce01ed49591582aaec6
Jean Michel <jean.michel@example.com>
2025-12-06 11:28:58 +0300

    fix project

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // ⚠️ Ignore les erreurs ESLint pendant le build
-        ignoreDuringBuilds: true,
-    },
     typescript: {
-        // ⚠️ Ignore les erreurs TypeScript (si nécessaire)
         ignoreBuildErrors: true,
     },
     images: {
